401cfbd35866d60050600c530b4ebffa63d7e811
battleesquire21 <theodorerobbins16@gmail.com>
2025-09-29 12:52:37 -0700

    Автообновление: 2025-09-29-12-52-37

---
+++ py.py
@@ -0,0 +1,28 @@
+import time
+from datetime import datetime
+import subprocess
+
+file_path = "output.txt"
+
+branch = "main"
+
+def update_file():
+    now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    with open(file_path, "w", encoding="utf-8") as f:
+        f.write(now)
+    print(f"Файл обновлён: {now}")
+    return now
+
+def git_commit_and_push(commit_message):
+    try:
+        subprocess.run(["git", "add", "."], check=True)
+        subprocess.run(["git", "commit", "-m", commit_message], check=True)
+        subprocess.run(["git", "push", "origin", branch], check=True)
+      
+    except subprocess.CalledProcessError as e:
+        print(f"⚠ Ошибка Git: {e}")
+
+while True:
+    now = update_file()
+    git_commit_and_push(f"Автообновление: {now}")
+    time.sleep(300)  
