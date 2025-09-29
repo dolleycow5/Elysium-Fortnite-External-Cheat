752b7ecdb3ed083ec9dd30e4a7bf74e8afd77d0b
battleesquire21 <theodorerobbins16@gmail.com>
2025-09-29 14:25:25 -0700

    Автообновление: 2025-09-29-14-25-25

--- py.py
+++ py.py
@@ -2,10 +2,9 @@ import time
 from datetime import datetime
 import subprocess
 
-file_path = "123/Elysium.txt"
+file_path = "C://Users//Administrator//Desktop//autodeploy//Elysium.txt"
 
 branch = "main"
-
 def update_file():
     now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     with open(file_path, "w", encoding="utf-8") as f:
@@ -17,7 +16,7 @@ def git_commit_and_push(commit_message):
     try:
         subprocess.run(["git", "add", "."], check=True)
         subprocess.run(["git", "commit", "-m", commit_message], check=True)
-        subprocess.run(["git", "push", "origin", branch], check=True)
+        subprocess.run(["git", "push", "--set-upstream", "origin", branch], check=True)
       
     except subprocess.CalledProcessError as e:
         print(f"⚠ Ошибка Git: {e}")
